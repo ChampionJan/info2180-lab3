f8f20a7d2bf51d059acb9f2bfb9d7800c7e00f78
Janai Shelton <janai.shelton@mymona.uwi.edu>
2022-10-06 18:11:35 -0500

    Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,27 @@
 window.addEventListener("load", (event)=> {
-    let squares  = document.querySelectorAll("#board > div")
-    squares.forEach((square)=>{
+    let squares  = document.querySelectorAll("#board > div");
+    let state = -1;
+
+    squares.forEach((square)=> {
         square.classList.add("square");
+        square.innerHTML = " ";
+
+    });
+
+    squares.forEach((square)=> {
+        square.onclick = (event)=>{
+            if (state == -1 || state == 1){
+                square.classList.remove("O");
+                square.classList.add("X");
+                square.innerHTML = 'X';
+                state = 0;
+            }
+            else {
+                square.classList.remove("X");
+                square.classList.add("O");
+                square.innerHTML = 'O';
+                state = 1;
+            }
+        }  
     });
 });
